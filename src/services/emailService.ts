93ea26598311f23f7a43b09c0e8e6080e2c10d6f
Daniel Arenas <daniel1144151@gmail.com>
2026-06-26 12:49:53 -0500

    follow vars

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -5,6 +5,9 @@ const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
 const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
 
 export const sendNotification = (sceneName: string) => {
+  console.log(`SERVICE_ID ${SERVICE_ID}`);
+  console.log(`TEMPLATE_ID ${TEMPLATE_ID}`);
+  console.log(`PUBLIC_KEY ${PUBLIC_KEY}`);
   emailjs.send(SERVICE_ID, TEMPLATE_ID, {
     message: `Isa ha llegado a: ${sceneName} a las ${new Date().toLocaleTimeString()}`,
   }, PUBLIC_KEY)
