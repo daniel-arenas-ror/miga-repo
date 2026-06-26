4ee9d5ebb10b6b3da2365880e83527abde560f68
Daniel Arenas <daniel1144151@gmail.com>
2026-06-26 12:59:58 -0500

    update final scene

--- src/components/FinalScene.tsx
+++ src/components/FinalScene.tsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 
 export const FinalScene = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [buttonActive, setButtonActive] = useState(false);
 
   const moveButton = () => {
     // Aumentamos un poco el rango para que sea más divertido
@@ -17,20 +18,36 @@ export const FinalScene = () => {
         ¿Qué dices? Me encantaría que charláramos ...
       </h2>
 
-      {/* Botón de WhatsApp - Ahora azul vibrante */}
-      <a
-        href="https://wa.me/+573002467379?text=Hola%20vamos%20por%20un%20Helado/Cafe"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="mb-8 px-8 py-4 bg-blue-500 text-white rounded-2xl font-bold text-lg shadow-lg hover:bg-blue-600 transition-all active:scale-95"
-      >
-        ¡Vamos por ese café! ☕
-      </a>
+      {buttonActive == false && (
+        <motion.button
+          transition={{ type: "spring", stiffness: 300, damping: 10 }}
+          onClick={() => setButtonActive(!buttonActive)}
+          className="px-6 py-2 bg-blue-300 text-blue-800 rounded-full font-medium"
+        >
+          ¡Vamos por ese café! ☕
+        </motion.button>
+      )}
+
+      {buttonActive == true && (
+        <>
+          <p className="text-blue-900 mb-8">
+            no puedes tomar cafe..... entonces mejor.....
+          </p>
+          <a
+            href="https://wa.me/+573002467379?text=Hola%20vamos%20por%20un%20Cafe"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mb-8 px-8 py-4 bg-blue-500 text-white rounded-2xl font-bold text-lg shadow-lg hover:bg-blue-600 transition-all active:scale-95"
+          >
+            Vamos por un Helado 🍦
+          </a>
+        </>
+      )}
 
       {/* Botón de Cancelar - Contrastando sutilmente */}
       <motion.button
         animate={{ x: position.x, y: position.y }}
-        transition={{ type: "spring", stiffness: 300, damping: 10 }}
+        transition={{ type: "spring", stiffness: 300, damping: 10, duration: 0.1 }}
         onMouseEnter={moveButton} // Se mueve al acercar el mouse (PC)
         onClick={moveButton} // Se mueve al tocar (Móvil)
         className="px-6 py-2 bg-blue-200 text-blue-800 rounded-full font-medium"
